c022d107a20830017a91bd18a714826c26c7b3af
Kaspars <kasparsj@gmail.com>
2016-10-07 20:12:07 +0300

    added Abstract->getAuthors()

--- src/Scopus/Response/Abstracts.php
+++ src/Scopus/Response/Abstracts.php
@@ -10,6 +10,9 @@ class Abstracts
     /** @var AbstractCoredata */
     protected $coredata;
     
+    /** @var AbstractAuthor[] */
+    protected $authors;
+    
     /** @var AbstractItem */
     protected $item;
     
@@ -21,6 +24,32 @@ class Abstracts
     public function getCoredata()
     {
         return $this->coredata ?: $this->coredata = new AbstractCoredata($this->data['coredata']);
+    }
+
+    /**
+     * @return AbstractAuthor[]
+     */
+    public function getAuthors()
+    {
+        if (isset($this->data['authors']['author'])) {
+            return $this->authors ?: $this->authors = array_map(function($author) {
+                return new AbstractAuthor($author);
+            }, $this->data['authors']['author']);
+        }
+    }
+
+    /**
+     * @param $name
+     * @return AbstractAuthor
+     */
+    public function findAuthorByName($name)
+    {
+        $matches = array_filter($this->getAuthors(), function(AbstractAuthor $author) use ($name) {
+            return $author->getIndexedName() === $name;
+        });
+        if ($matches) {
+            return array_values($matches)[0];
+        }
     }
     
     public function getItem()
