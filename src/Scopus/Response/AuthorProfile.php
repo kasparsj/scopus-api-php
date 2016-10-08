eea22f44f40f2a79a41c987a1138ff45731b4330
Kaspars <kasparsj@gmail.com>
2016-10-08 15:34:08 +0300

    allow name-variants to be optional

--- src/Scopus/Response/AuthorProfile.php
+++ src/Scopus/Response/AuthorProfile.php
@@ -27,6 +27,6 @@ class AuthorProfile
     {
         return $this->nameVariants ?: $this->nameVariants = array_map(function($nameVariant) {
             return new AuthorName($nameVariant);
-        }, $this->data['name-variant']);
+        }, isset($this->data['name-variant']) ? $this->data['name-variant'] : []);
     }
 }
