8ffdea4a6985d248245c08791fbd234ec8f47129
Kaspars <kasparsj@gmail.com>
2016-10-22 12:36:45 +0300

    added affiliation methods to author

--- src/Scopus/Response/Affiliation.php
+++ src/Scopus/Response/Affiliation.php
@@ -20,25 +20,32 @@ class Affiliation
         return $this->data['afid'];
     }
     
+    public function getUrl()
+    {
+        return isset($this->data['affiliation-url']) ? $this->data['affiliation-url'] : null;
+    }
+    
     public function getName()
     {
-        return $this->data['affilname'];
+        return isset($this->data['affilname']) ? $this->data['affilname'] : null;
     }
     
     public function getCity()
     {
-        return $this->data['affiliation-city'];
+        return isset($this->data['affiliation-city']) ? $this->data['affiliation-city'] : null;
     }
     
     public function getCountry()
     {
-        return $this->data['affiliation-country'];
+        return isset($this->data['affiliation-country']) ? $this->data['affiliation-country'] : null;
     }
     
     public function getNameVariant()
     {
-        return $this->nameVariants ?: $this->nameVariants = array_map(function($nameVariant) {
-            return $nameVariant['$'];
-        }, $this->data['name-variant']);
+        if (isset($this->data['name-variant'])) {
+            return $this->nameVariants ?: $this->nameVariants = array_map(function($nameVariant) {
+                return $nameVariant['$'];
+            }, $this->data['name-variant']);
+        }
     }
 }
--- src/Scopus/Response/Author.php
+++ src/Scopus/Response/Author.php
@@ -7,6 +7,12 @@ class Author
     /** @var array */
     protected $data;
     
+    /** @var Affiliation */
+    protected $affiliation;
+    
+    /** @var Affiliation[] */
+    protected $affiliation_history;
+    
     /** @var AuthorProfile */
     protected $profile;
 
@@ -15,8 +21,31 @@ class Author
         $this->data = $data;
     }
     
+    public function getAffiliation()
+    {
+        return $this->affiliation ?: $this->affiliation = new Affiliation($this->prepareAffiliationData($this->data['affiliation-current']));
+    }
+    
+    public function getAffiliationHistory()
+    {
+        if (isset($this->data['affiliation-history'])) {
+            return $this->affiliation_history ?: $this->affiliation_history = array_map(function($affiliation) {
+                return new Affiliation($this->prepareAffiliationData($affiliation));
+            }, $this->data['affiliation-history']);
+        }
+    }
+    
     public function getProfile()
     {
         return $this->profile ?: $this->profile = new AuthorProfile($this->data['author-profile']);
     }
+    
+    protected function prepareAffiliationData($affiliation)
+    {
+        return [
+            'afid' => $affiliation['@id'],
+            'affiliation-url' => $affiliation['@href']
+        ];
+    }
+    
 }
