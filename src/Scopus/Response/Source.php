760141e614c9292416ef7125e147eed04f2a10ad
Kaspars <kasparsj@gmail.com>
2017-03-17 11:54:14 +0200

    added volume and issue getters to Source

--- src/Scopus/Response/Source.php
+++ src/Scopus/Response/Source.php
@@ -44,4 +44,22 @@ class Source
         $pageRange = $this->getPageRange();
         return $pageRange && isset($pageRange['@last']) ? $pageRange['@last'] : null;
     }
+    
+    protected function getVoliss()
+    {
+        $volisspag = $this->getVolisspag();
+        return $volisspag && isset($volisspag['voliss']) ? $volisspag['voliss'] : null;
+    }
+    
+    public function getIssue()
+    {
+        $volIss = $this->getVoliss();
+        return $volIss && isset($volIss['@issue']) ? $volIss['@issue'] : null;
+    }
+    
+    public function getVolume()
+    {
+        $volIss = $this->getVoliss();
+        return $volIss && isset($volIss['@volume']) ? $volIss['@volume'] : null;
+    }
 }
